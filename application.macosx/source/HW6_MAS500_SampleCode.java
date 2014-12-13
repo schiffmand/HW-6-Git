e639f94871655a5b63f57dad6d1431ec774d2d84
schiffmand <dan@Gluten-Free-SMacbook.local>
2014-12-13 18:12:19 -0500

    Created HW6 Gitting Everything
    Git it up!

---
+++ application.macosx/source/HW6_MAS500_SampleCode.java
@@ -0,0 +1,38 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class HW6_MAS500_SampleCode extends PApplet {
+
+public void setup() {
+  size(480, 120);
+}
+
+public void draw() {
+  if (mousePressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "HW6_MAS500_SampleCode" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
